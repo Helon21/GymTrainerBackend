4c2eda9a9535f3ca2371617d11c38a795c10e352
Helon21 <helonbentes51@hotmail.com>
2025-03-24 21:01:07 -0300

    starting repository and configure `docker-compose file`

---
+++ src/main/java/com/example/GymTrainer/GymTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.GymTrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GymTrainer/GymTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GymTrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
